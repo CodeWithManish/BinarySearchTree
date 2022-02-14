928a2fcfb4e313bca5c3860bee41f905127fd007
Manish <manishkumar0156@gmail.com>
2022-02-14 23:58:43 +0530

    [Manish] Add: Search into binary search tree

--- BinarySearchTree/BinarySearchTrees.cs
+++ BinarySearchTree/BinarySearchTrees.cs
@@ -55,6 +55,32 @@ namespace BinarySearchTree
             Console.WriteLine("Size " + " " + (1 + this.leftCount + this.rightCount));
         }
 
+        public bool IfExists(T element, BinarySearchTrees<T> node)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+            if (node.nodeData.Equals(element))
+            {
+                Console.WriteLine("Found the element in BST " + " " + node.nodeData);
+                result = true;
+            }
+            else
+            {
+                Console.WriteLine("Current element is {0} in BST ", node.nodeData);
+            }
+            if (element.CompareTo(node.nodeData) < 0)
+            {
+                IfExists(element, node.leftTree);
+            }
+            if (element.CompareTo(node.nodeData) > 0)
+            {
+                IfExists(element, node.rightTree);
+            }
+
+            return result;
+        }
         public void Display()
         {
             if (this.leftTree != null)
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -13,7 +13,7 @@ namespace BinarySearchTree
             Console.WriteLine("Welcome to Binary Search Tree Opration");
             BinarySearchTrees<int> tree = new BinarySearchTrees<int>(56);
 
-            Console.WriteLine("Enter 1.to value insertion\nEnter 2.to insert more Elements\n");
+            Console.WriteLine("Enter 1.to value insertion\nEnter 2.to insert more Elements\nEnter 3.to Search the Elements\n");
             int num = Convert.ToInt32(Console.ReadLine());
 
             switch (num)
@@ -39,6 +39,25 @@ namespace BinarySearchTree
                     tree.Insert(67);
                     tree.Display();
                     tree.GetSize();
+                    break;
+
+                case 3:
+                    tree.Insert(30);
+                    tree.Insert(70);
+                    tree.Insert(22);
+                    tree.Insert(40);
+                    tree.Insert(60);
+                    tree.Insert(95);
+                    tree.Insert(11);
+                    tree.Insert(65);
+                    tree.Insert(3);
+                    tree.Insert(16);
+                    tree.Insert(63);
+                    tree.Insert(67);
+                    tree.Display();
+                    tree.GetSize();
+                    bool result = tree.IfExists(63, tree);
+                    Console.WriteLine(result);
                     break;
 
                 default:
